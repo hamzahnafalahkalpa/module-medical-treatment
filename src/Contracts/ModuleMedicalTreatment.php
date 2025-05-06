3fc866c4b0e7cd297f296828614fd9bced6a6362
Hamzah <hamzahnafalah@gmail.com>
2025-05-06 14:26:21 +0700

    update modules

--- src/Contracts/ModuleMedicalTreatment.php
+++ src/Contracts/ModuleMedicalTreatment.php
@@ -2,6 +2,6 @@
 
 namespace Hanafalah\ModuleMedicalTreatment\Contracts;
 
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 interface ModuleMedicalTreatment extends DataManagement {}
