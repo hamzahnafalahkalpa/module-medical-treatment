f2734aa013a8a97a0319c233b0e8e2d935c418dc
Hamzah <hamzahnafalah@gmail.com>
2025-07-17 22:27:59 +0700

    upadte modules

--- src/Resources/MedicalTreatment/ViewMedicalTreatment.php
+++ src/Resources/MedicalTreatment/ViewMedicalTreatment.php
@@ -13,8 +13,12 @@ class ViewMedicalTreatment extends ViewExaminationStuff
       'name'              => $this->name,
       'treatment_code'    => $this->treatment_code ?? $this->medical_treatment_code,
       'treatment'         => $this->prop_treatment,
+      'label'             => $this->label,
+      'status'            => $this->status,
+      'created_at'        => $this->created_at,
+      'updated_at'        => $this->updated_at
     ];
-    $arr = $this->mergeArray(parent::toArray($request),$arr);
+    // $arr = $this->mergeArray(parent::toArray($request),$arr);
     return $arr;
   }
 }
